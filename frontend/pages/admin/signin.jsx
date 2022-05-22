f9ca0974790ffef774d713cc9f4339b7b763909d
chamathr <chamatht20@gmail.com>
2022-05-22 12:41:04 +0530

    changes

--- frontend/pages/admin/signin.jsx
+++ frontend/pages/admin/signin.jsx
@@ -2,10 +2,7 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
@@ -13,10 +10,11 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useForm, Controller } from "react-hook-form";
 import { Password } from '@mui/icons-material';
-import { InputLabel } from '@mui/material';
 import { useState } from 'react'
 import Select from "react-select"
 import React from "react"
+import * as Yup from 'yup';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 
 
 const Copyright = (props) => {
@@ -58,7 +56,7 @@ const SignIn = () => {
     //   }
     // )
 
-    console.log('beka')
+    console.log(gender)
 
     // const result = await res.json()
 
@@ -78,90 +76,78 @@ const SignIn = () => {
     setGender(e.value)
   }
 
+  // const handleGenderChange = (selectedGender, values) => {
+  //   setGender(selectedGender);
+  // };
+
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit(onSignIn)} sx={{ mt: 1 }}>
-            <TextField
-              {...register("userName")}
-              margin="normal"
-              fullWidth
-              id="userName"
-              label="User Name"
-              name="userName"
-              autoComplete="user name"
-              autoFocus
-              onChange={(e) => {
-                userName.onChange(e);
-                setName(e.target.value)
-              }
-              }
-            />
-            {errors.userName?.type === 'required' && <label style={{ color: 'red' }}>User name is required</label>}
-            {errors.userName?.type === 'minLength' && <label style={{ color: 'red' }}>Need to exceed min length</label>}
+    <div style={{ paddingLeft: "400px", paddingRight: "400px", paddingTop: "20px" }}>
+      <Formik
+        initialValues={{
+          name: '',
+          email: '',
+          password: '',
+          gender: ''
+        }}
+        validationSchema={Yup.object().shape({
+          name: Yup.string()
+            .required('Name is required'),
+          email: Yup.string()
+            .email('Email is invalid')
+            .required('Email is required'),
+          password: Yup.string()
+            .min(6, 'Password must be at least 6 characters')
+            .required('Password is required'),
+          gender: Yup.string()
+            .required('Gender is required'),
+        })}
+        onSubmit={fields => {
+          alert('SUCCESS!! :-)\n\n' + JSON.stringify(fields, null, 4))
+        }}
+        render={({ errors, status, touched }) => (
+          <Form>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <Field name="name" type="text" className={'form-control' + (errors.name && touched.name ? ' is-invalid' : '')} />
+              <ErrorMessage name="name" component="div" className="invalid-feedback" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <Field name="email" type="text" className={'form-control' + (errors.email && touched.email ? ' is-invalid' : '')} />
+              <ErrorMessage name="email" component="div" className="invalid-feedback" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <Field name="password" type="password" className={'form-control' + (errors.password && touched.password ? ' is-invalid' : '')} />
+              <ErrorMessage name="password" component="div" className="invalid-feedback" />
+            </div>
 
-
-            <TextField
-              {...register("password", { required: true, pattern: /^[A-Za-z]+$/i, type: Password })}
-              margin="normal"
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="password"
-            />
-            {errors.password?.type === 'required' && <label style={{ color: 'red' }}>Password is required</label>}
-
-            <Controller
-              {...register("gender", { required: true })}
-              name="gender"
-              control={control}
-              render={({ field:{ onChange }  }) => (
-                <Select
-                  isClearable
-                  isSearchable={false}
-                  className="react-dropdown"
-                  classNamePrefix="dropdown"
-                  instanceId="long-value-select"
-                  options={genderData}
-                  onChange={(e) => {
-                    onChange(e)
-                    handleDropdown(e)
+            <div className="form-group">
+              <label for="gender">Gender</label>
+              <Select
+                  placeholder=""
+                  value={gender}
+                  onChange={selectedOption => {
+                    setGender(selectedOption)
                   }}
+                  isSearchable={true}
+                  options={genderData}
+                  name="gender"
+                  isLoading={false}
+                  className={'form-control' + (errors.gender && touched.gender ? ' is-invalid' : '')}
                 />
-              )}
-            />
-            {errors.gender?.type === 'required' && <label style={{ color: 'red' }}>Option is required</label>}
+              <ErrorMessage name="gender" component="div" className="invalid-feedback" />
 
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
+            </div>
+
+            <div className="form-group" style={{ paddingTop: "20px" }}>
+              <button type="submit" className="btn btn-primary mr-2">Register</button>
+              <button type="reset" className="btn btn-secondary" style={{ marginLeft: "10px" }}>Reset</button>
+            </div>
+          </Form>
+        )}
+      />
+    </div>
   );
 }
 
